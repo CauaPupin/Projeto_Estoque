59cc11696508a08d6ae0995848904fa545711fd7
Cauã Pupin <cauapupin28@gmail.com>
2025-05-26 08:12:07 -0300

    att 26/05

--- Projeto_Estoque/Program.cs
+++ Projeto_Estoque/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Projeto_Estoque
 {
-    internal class Program
+     class Program
     {
         static void Main(string[] args)
         {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do produto");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade em Estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do Produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos para adicionar ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos para remover do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+
         }
     }
 }
